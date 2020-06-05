5ebefe582859f60766a743dc4ef84b247377826f
Brian Moses <bmoses00@stuy.edu>
2020-06-04 22:29:29 -0400

    Added map selection screen

--- app/static/js/Scenes/Scene0.js
+++ app/static/js/Scenes/Scene0.js
@@ -3,9 +3,21 @@ class Scene0 extends Phaser.Scene {
     super("selection");
   }
 
+  preload() {
+    this.load.image('ocean_road', 'static/images/maps/ocean_road.png');
+  }
   create () {
-    this.add.text(300, 300, 'pick a map (haha we picked for you we only have 1)');
-    this.scene.start('game', {map: 'ocean_road'});
+    this.add.text(200, 20, 'Select a Map!', { font: '60px Arial' });
+
+
+    let ocean_road = this.add.image(150, 150, 'ocean_road').setScale(.2);
+    this.add.text(82, 225, 'Ocean Road', { font: '24px Arial' });
+
+    ocean_road.setInteractive();
+    ocean_road.on('pointerdown', this.select_map, {scene: this, map: 'ocean_road'});
   }
 
+  select_map() {
+    this.scene.scene.start('game', {map: this.map});
+  }
 }
