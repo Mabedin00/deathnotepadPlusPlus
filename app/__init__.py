68afbc825b00e4aa69fca91808d5ff76f2db7b20
Eric Lam <elam00@stuy.edu>
2020-06-07 11:24:19 -0400

    skeleton for logging users

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template("game.html")
 
+
 if __name__ == "__main__":
-        app.debug = True
-        app.run()
+    app.debug = True
+    app.run()
