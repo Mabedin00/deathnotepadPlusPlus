5c2dcc3faa796889e98edb811460aa8ae0c9b5e2
Eric Lam <elam00@stuy.edu>
2020-06-07 13:34:31 -0400

    created database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./static/data/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello_world():
