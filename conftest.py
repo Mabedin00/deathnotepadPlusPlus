f4f62c82dbcb214a869d10443e9159dd01ad765d
Eric Lam <elam00@stuy.edu>
2020-06-07 23:43:56 -0400

    refactored for unit testing, added authentication tests

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import os, tempfile, shutil, pytest
+from app import app, db
+
+
+@pytest.fixture
+def application():
+    db_fd, temp_path = tempfile.mkstemp('.db')
+    shutil.copy2('./app/static/data/database.db', temp_path)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{temp_path}'
+    app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
+
+    with app.app_context():
+        db.create_all()
+    yield app
+
+    os.close(db_fd)
+
+
+@pytest.fixture
+def client(application):
+    return application.test_client()
