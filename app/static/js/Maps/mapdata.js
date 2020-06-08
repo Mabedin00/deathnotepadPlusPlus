67b10c198783231ed30910df75eb7372e23e5513
Brian Moses <bmoses00@stuy.edu>
2020-06-08 12:42:25 -0400

    added support for multiple paths

--- app/static/js/Maps/mapdata.js
+++ app/static/js/Maps/mapdata.js
@@ -2,15 +2,53 @@ var map_data = {
   'ocean_road': {
     'tiles': ocean_road,
     'type': 'linear',
+    'num_paths': 1,
     'xlist': [620, 620, 500, 500, 80, 80,  166, 171, 249, 249, 334, 334, 414, 414, 516, 516, 620, 620, 319, 319, -40],
     'ylist': [0,   148, 148, 88,  88, 248, 248, 184, 184, 254, 254, 185, 185, 335, 335, 236, 236, 400, 400, 340, 340]
   },
   'floating_island': {
     'tiles': floating_island,
     'type': 'catmull',
+    'num_paths': 1,
     'xlist': [0,   17,  33,  50,  66,  105, 145, 183, 223, 258, 283, 294, 297, 314, 360, 403, 444, 481, 494, 487, 465, 444, 444, 458, 480, 514, 547, 582, 612, 626, 626, 614, 590, 560, 538, 507, 481, 455, 441, 427, 404, 373, 341, 308, 280, 250, 219, 188, 152, 119, 77 , 53,  21,  0],
     'ylist': [382, 373, 364, 357, 348, 330, 311, 303, 302, 315, 341, 377, 408, 436, 449, 450, 440, 421, 390, 356, 320, 286, 259, 233, 212, 201, 198, 191, 171, 141, 111, 81,  62,  51,  44,  49,  54,  72,  96,  121, 135, 146, 152, 159, 159, 162, 163, 163, 164, 168, 175, 180, 190, 198]
-  }
+  },
+  'castle': {
+    'tiles': castle,
+    'type': 'catmull',
+    'num_paths': 2,
+    'xlist' : [],
+    'ylist' : [],
+    'xlist1': [],
+    'ylist1': [],
+  },
+  'lightning_scar': {
+    'tiles': lightning_scar,
+    'type': 'linear',
+    'num_paths': 2,
+    'xlist': [],
+    'ylist': [],
+    'xlist1': [],
+    'ylist1': [],
+  },
+  'space_trucking': {
+    'tiles': space_trucking,
+    'type': 'catmull',
+    'num_paths': 1,
+    'xlist': [],
+    'ylist': [],
+  },
+  'scorched_earth': {
+    'tiles': scorched_earth,
+    'type': 'catmull',
+    'num_paths': 3,
+    'xlist': [],
+    'ylist': [],
+    'xlist1': [],
+    'ylist1': [],
+    'xlist2': [],
+    'ylist2': [],
+  },
 }
 // 'bloons' : [Red, Blue, Green, Yellow, Pink, White, Black, Zebra, Rainbow, Ceramic, MOAB]
 var level_data = [
