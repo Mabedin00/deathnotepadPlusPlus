390b906ea1e68cb3751899e59e77a6e9e4a457bb
Eric Lam <elam00@stuy.edu>
2020-06-11 13:34:50 -0400

    refactored leaderboards

--- dethpad/__init__.py
+++ dethpad/__init__.py
@@ -6,7 +6,6 @@ from flask_login import LoginManager, current_user
 from dethpad.models import db, User, Map0, Map1, Map2, Map3, Map4, Map5
 from dethpad.auth import auth
 
-
 app = Flask(__name__)
 app.register_blueprint(auth)
 app.secret_key = os.urandom(32)
@@ -17,7 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 
 db.init_app(app)
 with app.app_context():
-   db.create_all()
+    db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -36,10 +35,8 @@ def root():
     current_id = -1
     if current_user.is_authenticated:
         current_id = current_user.id
-    # print(current_user)
-    # print(current_user.id)
 
-    return render_template("game.html", id = current_id)
+    return render_template("game.html", id=current_id)
 
 
 @app.route('/test')
@@ -49,61 +46,55 @@ def test():
 
 @app.route('/score', methods=['POST'])
 def score():
-    data = request.get_json(force = True)
+    data = request.get_json(force=True)
     if data['map'] == 'ocean_road':
         entry = Map0(data['score'], data['id'])
-        db.session.add(entry)
-        db.session.commit()
     elif data['map'] == 'cubism':
         entry = Map1(data['score'], data['id'])
-        db.session.add(entry)
-        db.session.commit()
     elif data['map'] == 'floating_island':
         entry = Map2(data['score'], data['id'])
-        db.session.add(entry)
-        db.session.commit()
     elif data['map'] == 'lightning_scar':
         entry = Map3(data['score'], data['id'])
-        db.session.add(entry)
-        db.session.commit()
     elif data['map'] == 'castlemania':
         entry = Map4(data['score'], data['id'])
-        db.session.add(entry)
-        db.session.commit()
     elif data['map'] == 'scorched_earth':
         entry = Map5(data['score'], data['id'])
-        db.session.add(entry)
-        db.session.commit()
+    db.session.add(entry)
+    db.session.commit()
+    return ''
 
-    return 'hi'
 
 @app.route('/leaderboard')
 def leaderboard():
+    scores = {}
+    if current_user.is_authenticated:
+        scores['Ocean Road'] = [get_score(current_user.map0_scores.order_by(Map0.score.desc()).first())]
+        scores['Cubism'] = [get_score(current_user.map1_scores.order_by(Map1.score.desc()).first())]
+        scores['Floating Island'] = [get_score(current_user.map2_scores.order_by(Map2.score.desc()).first())]
+        scores['Lightning Scar'] = [get_score(current_user.map3_scores.order_by(Map3.score.desc()).first())]
+        scores['Castlemania'] = [get_score(current_user.map4_scores.order_by(Map4.score.desc()).first())]
+        scores['Scorched Earth'] = [get_score(current_user.map5_scores.order_by(Map5.score.desc()).first())]
+    else:
+        flash('You are not logged in!', 'warning')
+        scores['Ocean Road'] = [0]
+        scores['Cubism'] = [0]
+        scores['Floating Island'] = [0]
+        scores['Lightning Scar'] = [0]
+        scores['Castlemania'] = [0]
+        scores['Scorched Earth'] = [0]
 
-    if (not current_user.is_authenticated):
-        flash('You are not logged in', 'danger')
-        return redirect(url_for("root"))
+    scores['Ocean Road'].append(get_score(Map0.query.order_by(Map0.score.desc()).first()))
+    scores['Cubism'].append(get_score(Map1.query.order_by(Map1.score.desc()).first()))
+    scores['Floating Island'].append(get_score(Map2.query.order_by(Map2.score.desc()).first()))
+    scores['Lightning Scar'].append(get_score(Map3.query.order_by(Map3.score.desc()).first()))
+    scores['Castlemania'].append(get_score(Map4.query.order_by(Map4.score.desc()).first()))
+    scores['Scorched Earth'].append(get_score(Map5.query.order_by(Map5.score.desc()).first()))
 
-    user_scores = []
-    global_scores = []
-    user_scores.append(get_score(current_user.map0_scores.order_by(Map0.score.desc()).first()))
-    user_scores.append(get_score(current_user.map1_scores.order_by(Map1.score.desc()).first()))
-    user_scores.append(get_score(current_user.map2_scores.order_by(Map2.score.desc()).first()))
-    user_scores.append(get_score(current_user.map3_scores.order_by(Map3.score.desc()).first()))
-    user_scores.append(get_score(current_user.map4_scores.order_by(Map4.score.desc()).first()))
-    user_scores.append(get_score(current_user.map5_scores.order_by(Map5.score.desc()).first()))
+    return render_template('leaderboard.html', scores=scores)
 
-    global_scores.append(get_score(Map0.query.order_by(Map0.score.desc()).first()))
-    global_scores.append(get_score(Map1.query.order_by(Map1.score.desc()).first()))
-    global_scores.append(get_score(Map2.query.order_by(Map2.score.desc()).first()))
-    global_scores.append(get_score(Map3.query.order_by(Map3.score.desc()).first()))
-    global_scores.append(get_score(Map4.query.order_by(Map4.score.desc()).first()))
-    global_scores.append(get_score(Map5.query.order_by(Map5.score.desc()).first()))
-
-    return render_template('leaderboard.html', user_scores = user_scores, global_scores = global_scores)
 
 def get_score(score):
-    if (score is None):
+    if score is None:
         return 0
     else:
         return score.score
